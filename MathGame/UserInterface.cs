41cbada6c297b148950384c63086cd064250d07e
lari <popoxta@gmail.com>
2025-03-20 06:47:36 +1300

    Create UserInterface class and add Welcome method

---
+++ MathGame/UserInterface.cs
@@ -0,0 +1,21 @@
+﻿using Spectre.Console;
+
+namespace MathGame;
+
+public class UserInterface
+{
+    private enum GameOptions
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division,
+        Exit
+    }
+
+    public void Welcome()
+    {
+        AnsiConsole.MarkupLine("Welcome to the interplanetary Maths Quiz!");
+    }
+    
+}
